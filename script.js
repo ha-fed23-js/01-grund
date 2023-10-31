aed9e6b80805cd485c9540648cf4ffd5c737b0a2
David Andersson <lejonmanen@gmail.com>
2023-10-31 12:58:59 +0100

    Exempel if

---
+++ script.js
@@ -0,0 +1,26 @@
+// Här börjar programmet
+// =, ==, ===
+let age = 9;
+// age = age - 10
+if(     age < 7     ) {
+	console.log('Du är för liten för att åka Balder')
+} else if( age === 9 ) {
+	console.log('Nio år, grattis')
+} else {
+	console.log('Varsågod och åk!')
+}
+console.log('Nu är det slut')
+
+
+// Uppgift: Skapa en upprepning som skriver ut talen 1 till 10
+// ange en variabel x till 1
+// upprepa 10 gånger
+// skriv ut x
+// öka värdet på x med 1
+/*
+let x = 1;
+for (let count = 0; count < 10; count++) {
+	console.log(x)
+	x = x + 1;
+}
+*/
